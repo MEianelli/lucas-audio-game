d1bb213b74e87b4e89213914e65504547ba7a0d9
MEianelli <filipelouzano@gmail.com>
2025-03-14 10:37:54 -0300

    supabase file cleaned

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://hiinnoepvfmkkdioyanc.supabase.co";
-const supabaseKey =
-  process.env.SUPABASE_ANON_KEY ||
-  (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
+const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
-export const storageBaseUrl =
-  "https://hiinnoepvfmkkdioyanc.supabase.co/storage/v1/object/public";
+export const storageBaseUrl = `${supabaseUrl}/storage/v1/object/public`;
