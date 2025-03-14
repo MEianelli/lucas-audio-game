815b2e2e3f5f0dec2587839dd9be5aea81548edf
MEianelli <filipelouzano@gmail.com>
2025-03-14 10:12:02 -0300

    clean up

--- src/components/custom/Admin/GenerateRandomCard.tsx
+++ src/components/custom/Admin/GenerateRandomCard.tsx
@@ -1,21 +1,8 @@
-import { useState } from "react";
 import { FlexC } from "@/components/containers/flex";
-import { getRandomMoviesWithMedia } from "@/lib/supabase";
 import { Button } from "@/components/buttons/buttons";
 import { Text } from "@/components/text/text";
-import { RndMovie } from "@/types/types";
-import { GuessCard } from "../GuessCard/GuessCard";
 
 export const GenerateRandomCard = () => {
-  const [guess, setGuess] = useState<RndMovie | null>(null);
-
-  async function handleGenerate() {
-    const data = await getRandomMoviesWithMedia([], 8);
-    if (data?.[0]) {
-      setGuess(data[0]);
-    }
-  }
-
   return (
     <FlexC
       css={{
@@ -25,11 +12,8 @@ export const GenerateRandomCard = () => {
         alignItems: "center",
       }}
     >
-      <Button variant={"delete"} onClick={handleGenerate}>
-        Generate
-      </Button>
+      <Button variant={"delete"}>Generate</Button>
       <Text size={"b"}>Gerar card para vizualizacao:</Text>
-      <GuessCard key={guess?.movie_id} card={guess!} isInView={true} />
     </FlexC>
   );
 };
