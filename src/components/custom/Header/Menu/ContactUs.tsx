1020057468dac8f04b1878005ccdbfcc18c7e538
MEianelli <filipelouzano@gmail.com>
2025-03-23 16:12:56 -0300

    adding lgitch

--- src/components/custom/Header/Menu/ContactUs.tsx
+++ src/components/custom/Header/Menu/ContactUs.tsx
@@ -1,8 +1,13 @@
 import { ButtonWhite } from "@/components/buttons/buttons";
+import { Glitch } from "@/components/buttons/GlitchTexts/Version1/glitch";
 
 export const ContactUs = () => {
   function handleClick() {
     console.log("Ola");
   }
-  return <ButtonWhite onClick={handleClick}>Contact Us</ButtonWhite>;
+  return (
+    <ButtonWhite onClick={handleClick}>
+      <Glitch title="Contact Us" />
+    </ButtonWhite>
+  );
 };
--- src/pages/tests.tsx
+++ src/pages/tests.tsx
@@ -1,3 +1,4 @@
+import { Glitch } from "@/components/buttons/GlitchTexts/Version1/glitch";
 import { Div } from "@/components/containers/div";
 
 const Tests = () => {
@@ -8,50 +9,10 @@ const Tests = () => {
         width: "900px",
         height: "900px",
         alignContent: "center",
-      }}
-    >
-      <Div
-        css={{
-          background: "$white",
-          width: "90px",
-          height: "90px",
-          margin: "0 auto",
-        }}
-      >
-        <svg width="10px" height="10px">
-          <circle fill="#0000ff" cx="10" cy="10" r="10" />
-        </svg>
-        <Div>AAAA</Div>
-      </Div>
-      <Div
-        css={{
-          background: "$purple",
-          width: "150px",
-          height: "150px",
-          margin: "0 auto",
-        }}
-      >
-        <svg viewBox="0 0 10 10" width="10px" height="10px">
-          <path d="M 5 10 A 1 1 0 0 0 5 0 A 1 1 0 0 0 5 10" fill="#ff0000" />
-        </svg>
-        <svg viewBox="0 0 10 10" width="20px" height="20px">
-          <path d="M 5 10 A 1 1 0 0 0 5 0 A 1 1 0 0 0 5 10" fill="#ff0000" />
-        </svg>
-        <svg viewBox="0 0 10 10" width="40px" height="40px">
-          <path d="M 5 10 A 1 1 0 0 0 5 0 A 1 1 0 0 0 5 10" fill="#ff0000" />
-        </svg>
-      </Div>
+      }}>
+      <Glitch title="asd" />
     </Div>
   );
 };
 
 export default Tests;
-
-/*
-  const { data, error } = await supabase.rpc("get_random_cards", {
-    num_cards: 10,
-    category_filter: "movie",
-  });
-
-  
-  */
